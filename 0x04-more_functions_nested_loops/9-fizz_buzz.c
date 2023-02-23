8a9733ee504c4d3bfbcee198478e8836405b4975
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-02-23 14:37:59 +0000

    The Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print fizz-Buzz
+ * Description: Fizz-Buzz test
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i ++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{	
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else if(i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
