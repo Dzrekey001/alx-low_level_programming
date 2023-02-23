97a4956034f427bc5993434ace45082cb3d60b6c
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-02-23 14:48:08 +0000

    The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,20 @@ int main(void)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("%s", "FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", "Buzz");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s", "Fizz");
+			printf("Fizz ");
 		}
 		else
 		{
-			printf("%i", i);
+			printf("%d ", i);
 		}
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
