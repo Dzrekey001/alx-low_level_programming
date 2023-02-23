6b1ebc7cdb982b882c5f9254545a6444bc19863f
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-02-23 01:36:27 +0000

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,38 +19,49 @@ void print_times_table(int n)
 			{
 				m = i * j;
 
-				if (m < 10)
-				{
-					if (!(j == 0))
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(m + '0');
-				}
-				else if (m < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar((m / 100) + '0');
-					_putchar((m / 10 % 10) + '0');
-					_putchar((m % 10) + '0');
-
-				}
-				if (!(j >= n))
-				{
-					_putchar(',');
-				}
+				putformat(m);
 			}
 			_putchar('\n');
 		}
 	}
+
+}
+/**
+ * putformat - format the time table
+ *
+ * @m: input paremeter
+ */
+
+
+void putformat(int m)
+{
+	if (m < 10)
+	{
+		if (!(j == 0))
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar(m + '0')
+	}
+	else if (m < 100)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar((m / 100) + '0');
+		_putchar((m / 10 % 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+		if (!(j >= n))
+		{
+			_putchar(',');
+		}
 
 }
