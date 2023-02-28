9a562d1950cfd4df04690eefc215d36c9d86751f
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-02-28 00:59:58 +0000

    a function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: parameter str
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int str_len;
+
+	for (str_len = 0; s[str_len] != '\0'; str_len++)
+	{
+		;
+	}
+
+	for (str_len = str_len - 1; str_len >= 0; str_len--)
+	{
+		_putchar(s[str_len]);
+	}
+}
