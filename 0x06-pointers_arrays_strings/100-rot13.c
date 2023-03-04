dadec48d7ce8052a2ce6475f0a785bb89b1351de
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-03-04 01:33:59 +0000

    function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,24 +7,18 @@
 
 char *rot13(char *s)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		while ((s[i] >= 'a' && s[i] <= 'z') ||
-			(s[i] >= 'A' && s[i] <= 'Z'))
+				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') ||
-				(s[i] >= 'A' && s[i] <= 'M'))
-			{
+					(s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
-			}
 			else
-			{
 				s[i] -= 13;
-			}
 			i++;
 		}
 		i++;
