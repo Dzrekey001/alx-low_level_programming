e5a832d1502182795d8a98f11e62f85fa55ba165
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-03-05 18:52:03 +0000

    a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,7 +11,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while (*s++ != '\0')
+	while (*s != '\0')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
 		{
@@ -20,6 +20,7 @@ char *_strpbrk(char *s, char *accept)
 				return (*s);
 			}
 		}
+		s++;
 	}
 	return (0);
 
