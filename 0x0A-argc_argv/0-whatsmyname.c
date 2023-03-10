4f5dbc786134e0c549347150d69e31e5c01b5458
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-03-10 17:19:35 +0000

    a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,13 +6,16 @@
  * Return: Always 0 (success)
  */
 
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; argv[0][i] != '\0'; i++)
+	i = 0;
+
+	while (argv[0] && argv[0][i] != '\0')
 	{
-		_putchar(argv[0][i]);
+		_putchar(argv[0][i++]);
 	}
+	_putchar('\n');
 	return (0);
 }
