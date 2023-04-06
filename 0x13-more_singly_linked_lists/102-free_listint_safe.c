80dd62018c40dd71d6709e8b36c0f78a844e2ffb
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-04-06 00:05:30 +0000

    a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,73 +1,51 @@
 #include "lists.h"
-
 /**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - frees a linked list.
- * @h: head of a list.
- *
- * Return: size of the list that was freed.
+ * free_listint_safe - frees a linked list and sets the head to NULL
+ * @h: double pointer to the head of the list
+ * Return: size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
+	size_t count = 0;
+	listint_t *current, *temp;
+	listint_t **addresses = NULL;
+	int i, j, found;
 
-	hptr = NULL;
+	if (h == NULL || *h == NULL)
+		return (0);
 	while (*h != NULL)
 	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
+		current = *h;
+		found = 0;
+		for (i = 0; i < count; i++)
 		{
-			add = add->next;
-			if (*h == add->p)
+			if (current == addresses[i])
 			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
+				found = 1;
+				break;
 			}
 		}
+		if (!found)
+		{
+			addresses = realloc(addresses, (count + 1) * sizeof(listint_t *));
+			if (addresses == NULL)
+				exit(98);
+			addresses[count] = current;
+			temp = current->next;
+			free(current);
+			count++;
+		}
+		else
+		{
+			*h = NULL;
+			break;
+		}
 
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
+		*h = temp;
 	}
-
+	for (j = 0; j < count; j++)
+		addresses[j] = NULL;
+	free(addresses);
 	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
+	return (count);
 }
-
