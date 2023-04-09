f8180bae5d174febc62591c8d65668229c692401
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-04-09 20:14:54 +0000

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
-	for (; text_content[len] != '\0'; len++)
+	if (text_content != NULL)
 	{
-		;
+		for (; text_content[len] != '\0'; len++)
+		{
+			;
+		}
 	}
 
 	opened_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
