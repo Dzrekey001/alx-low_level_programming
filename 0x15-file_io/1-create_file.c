3cd546e80de567e8fb9aaab9594f418b181cbc47
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-04-09 20:11:54 +0000

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	}
 
-	opened_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	opened_file = open(filename, O_CREAT | O_RDWR | O_TRUNC,0600);
 	write_content = write(opened_file, text_content, len);
 
 	if (opened_file == -1 || write_content == -1)
