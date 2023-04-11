600831b7f3593a5043895734481c9cd9bc39cdb1
Dzrekey Kwasi Daniel <dkdzrekey001@gmail.com>
2023-04-11 09:24:01 +0000

    crackme

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
 		write_c = write(to_file, buffer, read_c);
 		if (write_c == -1)
 			open_error("Error: Can't write to", argv[2], 99);
-	} while (read_c == 1024);
+	while (read_c == 1024);
 
 	close_c = close(from_file);
 	if (close_c == -1)
@@ -47,29 +47,23 @@ int main(int argc, char **argv)
 
 	return (0);
 }
-
 /**
  * close_error - error if file do not close
  * @message: error message
  * @FD_VALUE: return value of the file
  * @status: exit status
  */
-
 void close_error(char *message, int FD_VALUE, int status)
 {
 	dprintf(STDERR_FILENO, "%s %d\n", message, FD_VALUE);
 	exit(status);
 }
-
-
-
 /**
  * open_error - error if file do not close
  * @message: error message
  * @file_name: name of the file
  * @status: exit status
  */
-
 void open_error(char *message, char *file_name, int status)
 {
 	dprintf(STDERR_FILENO, "%s %s\n", message, file_name);
