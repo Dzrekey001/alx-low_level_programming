80a06bb9ff38e6023d45366077c5cc1946c95426
Daniel Kwasi Dzrekey <dkdzrekey001@gmail.com>
2023-06-10 18:01:10 +0000

    correction made to 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 		strlen(key) == 0 || value == NULL || key == NULL)
-		return (NULL);
+		return (0);
 
 	idx = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[idx];
